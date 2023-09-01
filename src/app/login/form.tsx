a2c9d933cebf7b1cd0d3082df99c1dd57b8afd0b
Dmitrii Kochnev <aurokkez@gmail.com>
2023-09-01 16:30:32 +0300

    wip: separate ui and account management apps

--- src/app/login/form.tsx
+++ src/app/login/form.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { redirect, useRouter } from 'next/navigation'
+import { redirect, useRouter, useSearchParams } from 'next/navigation'
 import { FormEvent, FormEventHandler, useState } from 'react'
 
-async function login(username: string, password: string): Promise<void> {
+async function login(username: string, password: string, loginRequestId: string): Promise<string> {
 
   const res = await fetch('https://localhost:20010/account/login', {
     method: 'POST',
@@ -15,24 +15,25 @@ async function login(username: string, password: string): Promise<void> {
     body: JSON.stringify({
       username: username,
       password: password,
+      loginRequestId: loginRequestId
     })
   })
   if (!res.ok) throw Error()
-
-  // todo
-  // – add type checking
-  return
+  const response = await res.json();
+  return response.loginResponseId
 }
 
 export default function Form() {
-
+  const searchParams = useSearchParams()
   const router = useRouter()
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    await login(username, password)
-    router.replace('/')
+    const loginRequestId = searchParams.get('loginRequestId') ?? ''
+    const loginResponseId = await login(username, password, loginRequestId)
+    const returnUrl = decodeURI(searchParams.get('ReturnUrl') ?? '/')
+    router.replace(returnUrl + `&loginResponseId=${loginResponseId}`)
   }
   return (
     <div>
